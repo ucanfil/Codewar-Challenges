e0ab1081b4b4efdad631949082e1514a88f3fd8a
Burak Tilek <burak.tilek@gmail.com>
2019-02-25 15:58:38 +0300

    Solutions are added

--- 6 kyu/Catalog/app.js
+++ 6 kyu/Catalog/app.js
@@ -1,32 +1,24 @@
 //====== S O L U T I O N - 1 ======//
+function catalog(s, article) {
+  s = s.split('\n\n')
+  const array = s.filter(item => item.includes(article))
 
-function DNAStrand(dna) {
-  return dna.split("").map(letter => letter === 'A' ? letter = 'T' : letter === 'T' ? letter = 'A' : letter === 'G' ? letter = 'C' : letter === 'C' ? letter = 'G' : "").join("");
+  const pattern1 = /<name[^>]*>((.|[\n\r])*)<\/name>/im
+  const pattern2 = /<prx[^>]*>((.|[\n\r])*)<\/prx>/im
+  const pattern3 = /<qty[^>]*>((.|[\n\r])*)<\/qty>/im
+
+  return array.length == 0 ? 'Nothing' : array.map(item => `${item.match(pattern1)[1]} > prx: $${item.match(pattern2)[1]} qty: ${item.match(pattern3)[1]}`).join('\r\n')
 }
 
 //====== S O L U T I O N - 2 ======//
+function catalog(s, article) {
+  const arr = []
+  s.split('\n+').forEach(line => {
+    if (line.includes(article)) {
+      const pattern = line.slice(12, -13).split(/<.*?><.*?>/)
+      arr.push(`${pattern[0]} > prx: $${pattern[1]} qty: ${pattern[2]}`)
+    }
+  })
 
-function DNAStrand(str) {
-  const pairs = {
-    A: 'T',
-    T: 'A',
-    G: 'C',
-    C: 'G'
-  };
-  return str.replace(/./g, function (letter) {
-    return pairs[letter];
-  });
-}
-
-//====== S O L U T I O N - 3 ======//
-
-function DNAStrand(str) {
-  const pairs = {
-    A: 'T',
-    T: 'A',
-    G: 'C',
-    C: 'G'
-  };
-
-  return str.split("").map(letter => pairs[letter]).join("");
+  return arr.length == 0 ? 'Nothing' : arr.join('/r/n')
 }
